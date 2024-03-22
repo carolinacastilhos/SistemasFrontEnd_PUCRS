a21bd0ad392db7aea6fa600f9a3eeb68b9e6d2c6
Carolina Castilhos <carolinacastilhos.vet@gmail.com>
2024-03-22 15:47:04 -0300

    changing home and about

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,10 +5,37 @@ const About = () => {
     <div className="d-flex justify-content-center align-items-center flex-column m-4">
       <h1>Sobre</h1>
       <p>
-        Esta é uma aplicação CRUD de carros HotWheels criada para a Fase 1 do
-        Projeto da disciplina de Desenvolvimento Front End do curso Análise e
-        Desenvolvimento de Sistemas na PUCRS.
+        CRUD é um acrônimo que representa as quatro operações básicas que podem
+        ser realizadas em dados persistentes em muitos sistemas de banco de
+        dados ou aplicações interativas. Essas operações são:
       </p>
+
+      <ol>
+        <li>
+          <b>Create (Criar):</b> Criar refere-se à operação de criar novos dados
+          ou registros no sistema. Isso geralmente envolve a inserção de novas
+          informações em um banco de dados ou a criação de novos objetos em um
+          sistema de software.
+        </li>
+        <li>
+          <b>Read (Ler):</b> Ler refere-se à operação de recuperar ou visualizar
+          dados existentes no sistema. Isso envolve geralmente a consulta ou
+          busca por dados armazenados em um banco de dados ou sistema de
+          arquivos.
+        </li>
+        <li>
+          <b>Update (Atualizar):</b> Atualizar refere-se à operação de modificar
+          dados existentes no sistema. Isso envolve a alteração ou atualização
+          de informações já presentes em um banco de dados ou sistema de
+          software.
+        </li>
+        <li>
+          <b>Delete (Excluir):</b> Excluir refere-se à operação de remover dados
+          existentes do sistema. Isso geralmente envolve a exclusão de registros
+          de um banco de dados ou a remoção de objetos de um sistema de
+          software.
+        </li>
+      </ol>
     </div>
   );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,11 @@ const Home = () => {
   return (
     <div className="d-flex justify-content-center align-items-center flex-column m-4">
       <h1>Página Inicial HotWheels</h1>
-      <p>Olá! Seja bem-vindo(a) ao CRUD de HotWheels!</p>
+      <p>
+        Olá! Seja bem-vindo(a) à aplicação CRUD de carros HotWheels criada para
+        a disciplina de Desenvolvimento Front End do curso Análise e
+        Desenvolvimento de Sistemas na PUCRS!
+      </p>
     </div>
   );
 };

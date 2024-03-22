433a16808c88955ef42b8b3a41a5ac9447d71954
Carolina Castilhos <carolinacastilhos.vet@gmail.com>
2024-03-22 17:12:17 -0300

    customizing home and about

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,17 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="d-flex justify-content-center align-items-center flex-column m-4">
       <h1>Página Inicial HotWheels</h1>
-      <p>
-        Olá! Seja bem-vindo(a) à aplicação CRUD de carros HotWheels criada para
-        a disciplina de Desenvolvimento Front End do curso Análise e
-        Desenvolvimento de Sistemas na PUCRS!
-      </p>
+      <div className="container boxHome">
+        <p>
+          Olá! Seja bem-vindo(a) à aplicação CRUD de carros HotWheels criada
+          para a disciplina de Desenvolvimento Front End do curso Análise e
+          Desenvolvimento de Sistemas na PUCRS!
+        </p>
+      </div>
     </div>
   );
 };
